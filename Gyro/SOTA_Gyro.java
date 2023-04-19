52e7c2867822e6327a396b336c715bf2f2a644d5
HoJo42 <howardwalz@gmail.com>
2023-04-18 17:39:25 -0700

    added documentation to the interface

--- Gyro/SOTA_Gyro.java
+++ Gyro/SOTA_Gyro.java
@@ -3,12 +3,38 @@ package lib.Gyro;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public interface SOTA_Gyro {
+    /**
+     * @return returns the angle of the main axis in Radians
+     */
     public double getAngle();
+
+    /**
+     * @return returns the Rotation2d of the main axis
+     */
     public Rotation2d getRotation2d();
-    public void setAngle(double angle);
-    public void setAngle(Rotation2d angle);
+
+    /**
+     * Sets the angle of the Gyro in Radians
+     */
+    public void setGyro(double radians);
+
+    /**
+     * Resets the angle of the gyro
+     */
     public void resetAngle();
+
+    /**
+     * @return Returns the Pitch of the gyro in Radians
+     */
     public double getPitch();
+
+    /**
+     * @return Returns the Roll of the gyro in Radians
+     */
     public double getRoll();
+
+    /**
+     * @return Returns the Yaw of the gyro in Radians
+     */
     public double getYaw();
 }

5f0db0ef4bf4de5d2e4b16927516e489f586451c
HoJo42 <howardwalz@gmail.com>
2023-07-15 20:51:45 -0400

    removed methods inherited from WPI motor interface

--- MotorController/SOTA_MotorController.java
+++ MotorController/SOTA_MotorController.java
@@ -1,33 +1,51 @@
 package SOTAlib.MotorController;
 
-
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import SOTAlib.Encoder.SOTA_Encoder;
 
 public interface SOTA_MotorController extends MotorController {
-    void setInverted(boolean isInverted);
-    boolean getInverted();
+
+    /**
+     * @param neutralOperation
+     */
     void setNeutralOperation(NeutralOperation neutralOperation);
+
     SOTA_Encoder getEncoder();
+
     double getEncoderVelocity();
+
     double getEncoderPosition();
+
     double getNativeEncoderVelocity();
+
     double getNativeEncoderPosition();
+
     double getNativeCountsPerRevolution();
+
     void resetNativeEncoder();
+
     double getMotorTemperature();
+
     double getMotorCurrent();
+
     void setCurrentLimit(int amps);
+
     MotorLimits getLimits();
+
     void setLimits(MotorLimits limits);
+
     double getLowerLimit();
+
     double getUpperLimit();
+
     boolean atUpperLimit();
+
     boolean atLowerLimit();
+
     void resetEncoder();
+
     boolean getNeutralOperation();
+
     void setNeutralOperation();
 
-
 }
-

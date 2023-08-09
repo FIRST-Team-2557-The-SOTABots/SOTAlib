1865eb6b840f1aac25267d616bbb2499460ddf5b
Howard Walz <howardwalz@gmail.com>
2023-08-08 21:23:49 -0700

    Factory should work

--- Factories/EncoderFactory.java
+++ Factories/EncoderFactory.java
@@ -5,11 +5,29 @@
 package SOTAlib.Factories;
 
 import SOTAlib.Config.EncoderConfig;
-import SOTAlib.Encoder.SOTA_Encoder;
+import SOTAlib.Encoder.Absolute.SOTA_AbsoulteEncoder;
+import SOTAlib.Encoder.Absolute.SOTA_AnalogEncoder;
+import SOTAlib.Encoder.Absolute.SOTA_DutyCycle;
+import SOTAlib.Encoder.Relative.QuadratureEncoder;
+import SOTAlib.Encoder.Relative.SOTA_RelativeEncoder;
+import SOTAlib.MotorController.NullConfigException;
 
 /** Add your docs here. */
 public class EncoderFactory {
-    public static SOTA_Encoder generateEncoder(EncoderConfig config) {
-        return (SOTA_Encoder) null; //TODO: Make work
+    public static SOTA_RelativeEncoder generateRelativeEncoder(EncoderConfig config) throws NullConfigException {
+        if (config == null) { throw new NullConfigException("EncoderFactory: Null Config");}
+        return new QuadratureEncoder(config);
+    }
+
+    public static SOTA_AbsoulteEncoder generateAbsoluteEncoder(EncoderConfig config) throws Exception {
+        if (config == null) {throw new NullConfigException("EncoderFacotry: Null Config");}
+        switch (config.getEncoderType()) {
+            case "DUTYCYCLE":
+                return new SOTA_DutyCycle(config); 
+            case "ANALOG":
+                return new SOTA_AnalogEncoder(config);
+            default:
+                throw new Exception("EncoderFactory: generateAbsoluteEncoder invaild encoder type, only accepts 'DUTYCYCLE' and 'ANALOG'");
+        }
     }
 }

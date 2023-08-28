4c9e5001a27bbb08e4f3edf2852e995faaabb94f
HoJo42 <howardwalz@gmail.com>
2023-08-28 11:13:04 -0700

    Should work

---
+++ Encoder/Absolute/SOTA_CANCoder.java
@@ -0,0 +1,48 @@
+package SOTAlib.Encoder.Absolute;
+
+import com.ctre.phoenix.sensors.CANCoder;
+
+import SOTAlib.Config.EncoderConfig;
+import SOTAlib.MotorController.NullConfigException;
+
+public class SOTA_CANCoder implements SOTA_AbsoulteEncoder {
+    private CANCoder mEncoder;
+
+    public SOTA_CANCoder(EncoderConfig config) throws NullConfigException {
+        if (config == null) {
+            throw new NullConfigException("SOTA_CANCoder: no config");
+        }
+
+        this.mEncoder = new CANCoder(config.getPort());
+
+        if (config.getEncoderOffset() != null) {
+            mEncoder.configMagnetOffset(config.getEncoderOffset());
+        }
+    }
+
+    @Override
+    public double getPosition() {
+        return mEncoder.getPosition() / 360;
+    }
+
+    @Override
+    public double getPositionNoOffset() {
+        return mEncoder.getAbsolutePosition() / 360;
+    }
+
+    @Override
+    public void setPositionOffset(double offset) {
+        mEncoder.configMagnetOffset(offset);
+    }
+
+    @Override
+    public double getPositionOffset() {
+        return mEncoder.configGetMagnetOffset();
+    }
+    @Override
+    public int getPort() {
+        return mEncoder.getDeviceID();
+    }
+
+
+}

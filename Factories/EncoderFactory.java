86c4a37963b5d99bac4162c744e4c429687a6973
HoJo42 <howardwalz@gmail.com>
2023-09-02 11:26:33 -0700

    added CANCoder to Factory

--- Factories/EncoderFactory.java
+++ Factories/EncoderFactory.java
@@ -7,6 +7,7 @@ package SOTAlib.Factories;
 import SOTAlib.Config.EncoderConfig;
 import SOTAlib.Encoder.Absolute.SOTA_AbsoulteEncoder;
 import SOTAlib.Encoder.Absolute.SOTA_AnalogEncoder;
+import SOTAlib.Encoder.Absolute.SOTA_CANCoder;
 import SOTAlib.Encoder.Absolute.SOTA_DutyCycle;
 import SOTAlib.Encoder.Relative.QuadratureEncoder;
 import SOTAlib.Encoder.Relative.SOTA_RelativeEncoder;
@@ -15,19 +16,26 @@ import SOTAlib.MotorController.NullConfigException;
 /** Add your docs here. */
 public class EncoderFactory {
     public static SOTA_RelativeEncoder generateRelativeEncoder(EncoderConfig config) throws NullConfigException {
-        if (config == null) { throw new NullConfigException("EncoderFactory: Null Config");}
+        if (config == null) {
+            throw new NullConfigException("EncoderFactory: Null Config");
+        }
         return new QuadratureEncoder(config);
     }
 
     public static SOTA_AbsoulteEncoder generateAbsoluteEncoder(EncoderConfig config) throws Exception {
-        if (config == null) {throw new NullConfigException("EncoderFacotry: Null Config");}
+        if (config == null) {
+            throw new NullConfigException("EncoderFacotry: Null Config");
+        }
         switch (config.getEncoderType()) {
             case "DUTYCYCLE":
-                return new SOTA_DutyCycle(config); 
+                return new SOTA_DutyCycle(config);
             case "ANALOG":
                 return new SOTA_AnalogEncoder(config);
+            case "CANCODER":
+                return new SOTA_CANCoder(config);
             default:
-                throw new Exception("EncoderFactory: generateAbsoluteEncoder invaild encoder type, only accepts 'DUTYCYCLE' and 'ANALOG'");
+                throw new Exception(
+                        "EncoderFactory: generateAbsoluteEncoder invaild encoder type, only accepts 'DUTYCYCLE', 'ANALOG', and 'CANCODER'");
         }
     }
 }

7598b7fa5115eb3991a2d4e1d70780652ea1687d
HoJo42 <howardwalz@gmail.com>
2023-09-30 20:56:18 -0700

    added module config

---
+++ Config/SwerveModuleConfig.java
@@ -0,0 +1,95 @@
+package SOTAlib.Config;
+
+public class SwerveModuleConfig {
+    private String name;
+    private MotorControllerConfig speedConfig;
+    private CompositeMotorConfig angleConfig;
+    private double angleP;
+    private double angleI;
+    private double angleD;
+    private double angleKs;
+    private double angleKv;
+    private double angleMaxVelocity;
+    private double angleMaxAcceleration;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MotorControllerConfig getSpeedConfig() {
+        return this.speedConfig;
+    }
+
+    public void setSpeedConfig(MotorControllerConfig speedConfig) {
+        this.speedConfig = speedConfig;
+    }
+
+    public CompositeMotorConfig getAngleConfig() {
+        return this.angleConfig;
+    }
+
+    public void setAngleConfig(CompositeMotorConfig angleConfig) {
+        this.angleConfig = angleConfig;
+    }
+
+    public double getAngleP() {
+        return this.angleP;
+    }
+
+    public void setAngleP(double angleP) {
+        this.angleP = angleP;
+    }
+
+    public double getAngleI() {
+        return this.angleI;
+    }
+
+    public void setAngleI(double angleI) {
+        this.angleI = angleI;
+    }
+
+    public double getAngleD() {
+        return this.angleD;
+    }
+
+    public void setAngleD(double angleD) {
+        this.angleD = angleD;
+    }
+
+    public double getAngleKs() {
+        return this.angleKs;
+    }
+
+    public void setAngleKs(double angleKs) {
+        this.angleKs = angleKs;
+    }
+
+    public double getAngleKv() {
+        return this.angleKv;
+    }
+
+    public void setAngleKv(double angleKv) {
+        this.angleKv = angleKv;
+    }
+
+    public double getAngleMaxVelocity() {
+        return this.angleMaxVelocity;
+    }
+
+    public void setAngleMaxVelocity(double angleMaxVelocity) {
+        this.angleMaxVelocity = angleMaxVelocity;
+    }
+
+    public double getAngleMaxAcceleration() {
+        return this.angleMaxAcceleration;
+    }
+
+    public void setAngleMaxAcceleration(double angleMaxAcceleration) {
+        this.angleMaxAcceleration = angleMaxAcceleration;
+    }
+
+}

153f2857d1ffce6668c2e19ad54f99b19c519873
Howard-Walz <howardwalz@gmail.com>
2023-12-26 20:42:21 -0800

    Changed to in-house analog encoder code

--- Encoder/Absolute/SOTA_AnalogEncoder.java
+++ Encoder/Absolute/SOTA_AnalogEncoder.java
@@ -2,41 +2,50 @@ package SOTAlib.Encoder.Absolute;
 
 import SOTAlib.Config.EncoderConfig;
 import SOTAlib.MotorController.NullConfigException;
-import edu.wpi.first.wpilibj.AnalogEncoder;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.AnalogInput;
 
 public class SOTA_AnalogEncoder implements SOTA_AbsoulteEncoder {
-    private AnalogEncoder mEncoder;
+    private AnalogInput mEncoder;
+    private double mOffset;
+    private final double MAX_VOLTAGE = 5.0;
 
     public SOTA_AnalogEncoder(EncoderConfig config) throws NullConfigException {
         if (config == null) {
             throw new NullConfigException("SOTA_AnalogEncoder: No config");
         }
 
-        this.mEncoder = new AnalogEncoder(config.getPort());
+        this.mEncoder = new AnalogInput(config.getPort());
 
         if (config.getEncoderOffset() != null) {
-            mEncoder.setPositionOffset(config.getEncoderOffset());
+            mOffset = config.getEncoderOffset();
         }
     }
 
     @Override
     public double getPosition() {
-        return mEncoder.get();
+        // angle encoder increases with cw movement, this conversion makes it increase
+        // with ccw movement
+        // for compatibility with the radian based setpoint from kinematics class
+        // -- Hayden Feeney (Robot2022)
+        return (-1 * MathUtil.inputModulus(mEncoder.getAverageVoltage() - mOffset, 0, MAX_VOLTAGE) + MAX_VOLTAGE)
+                / MAX_VOLTAGE;
     }
 
     @Override
     public double getPositionNoOffset() {
-        return mEncoder.getAbsolutePosition();
+        return (-1 * MathUtil.inputModulus(mEncoder.getAverageVoltage(), 0, MAX_VOLTAGE) + MAX_VOLTAGE)
+                / MAX_VOLTAGE;
     }
 
     @Override
     public void setPositionOffset(double offset) {
-        mEncoder.setPositionOffset(offset);
+        this.mOffset = offset;
     }
 
     @Override
     public double getPositionOffset() {
-        return mEncoder.getPositionOffset();
+        return mOffset;
     }
 
     @Override

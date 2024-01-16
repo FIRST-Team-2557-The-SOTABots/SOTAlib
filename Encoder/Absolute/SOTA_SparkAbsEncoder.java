9e5fd7147ac5a56bc81cc490d24ef507885a8d07
HoJo42 <howardwalz@gmail.com>
2024-01-16 14:20:43 -0800

    Sparkmax composite works with offset and invert

--- Encoder/Absolute/SOTA_SparkAbsEncoder.java
+++ Encoder/Absolute/SOTA_SparkAbsEncoder.java
@@ -6,35 +6,55 @@ public class SOTA_SparkAbsEncoder implements SOTA_AbsoulteEncoder {
 
     private AbsoluteEncoder mEncoder;
     private int sparkCanId;
+    private boolean inverted;
+    private double mOffset = 0.0;
 
-    public SOTA_SparkAbsEncoder(AbsoluteEncoder encoder, int sparkCanId) {
+    public SOTA_SparkAbsEncoder(AbsoluteEncoder encoder, int sparkCanId, boolean inverted, Double offset) {
         this.mEncoder = encoder;
         this.sparkCanId = sparkCanId;
+        this.inverted = inverted;
+        this.mOffset = offset;
     }
 
     @Override
     public double getPosition() {
-        return mEncoder.getPosition();
+        double output = 0;
+        if (inverted) {
+            output = 1 - (mEncoder.getPosition() - mOffset);
+        } else {
+            output = mEncoder.getPosition() - mOffset;
+        }
+        return output;
     }
 
     @Override
     public double getRawPosition() {
-        return mEncoder.getPosition() - mEncoder.getZeroOffset(); //TODO: Should test
+        return mEncoder.getPosition();
     }
 
     @Override
     public void setPositionOffset(double offset) {
-       mEncoder.setZeroOffset(offset); 
+       mOffset = offset; 
     }
 
     @Override
     public double getPositionOffset() {
-       return mEncoder.getZeroOffset(); 
+       return mOffset; 
     }
 
     @Override
     public int getPort() {
         return sparkCanId;
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+       this.inverted = isInverted; 
+    }
+
+    @Override
+    public boolean getInverted() {
+        return inverted;
     }
     
 }

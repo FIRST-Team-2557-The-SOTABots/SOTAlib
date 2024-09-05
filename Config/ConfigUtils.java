34d3b079d30713fa468fe38c11b0bd1ad5f22bc1
Howard-Walz <howardwalz@gmail.com>
2024-09-05 15:34:02 -0700

    Added javadoc to ObjectMapper

--- Config/ConfigUtils.java
+++ Config/ConfigUtils.java
@@ -9,17 +9,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.wpi.first.wpilibj.Filesystem;
 
+/**
+ * Maps json config files to config Objects
+ */
 public class ConfigUtils {
     private ObjectMapper mapper;
 
+    /**
+     * Creates a new ConfigUtils object with default ObjectMapper
+     */
     public ConfigUtils() {
         this(new ObjectMapper());
     }
 
+    /**
+     * Creates a new ConfigUtils object with specific ObjectMapper
+     *
+     * Don't use unless you know what you're doing.
+     */
     public ConfigUtils(ObjectMapper mapper) {
         this.mapper = mapper;
     }
 
+    /**
+     * @param <T> The type of Config Object you're mapping to (ex: MotorControllerConfig, EncoderConfig, etc.)
+     *
+     * @param clazz object class to map to (ex: MotorControllerConfig.class or MyCustomConfigObject.class)
+     *
+     * @param resource path to your json config file. The method automatically assumes your resorce is stored in a subdirectory of deploy/resources and ends in .json. Therefore the only information specificed in this String is the filename with extension omitted. If you have subdirectories for organization you must incude the subdirectory name first. (ex: to map from deploy/resources/ExampleSubdirectory/MyConfig.json the resource parameter should be "ExampleSubdirectory/MyConfig". To map from deploy/resources/UnorganizedConfig.json the resource parameter should be "UnorganizedConfig")
+     *
+     * @throws IOException throws if it fails to read the values. Stack trace is printed, check the filenames, good luck. :)
+     */
     public <T> T readFromClassPath(Class<T> clazz, String resource) throws IOException {
         try (BufferedReader br = new BufferedReader(
                 new FileReader(
@@ -41,3 +61,4 @@ public class ConfigUtils {
         }
     }
 }
+

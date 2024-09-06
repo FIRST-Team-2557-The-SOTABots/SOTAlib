9ad6472843bef8257f1d7c6dfa2fcc53ff9b2f51
Howard-Walz <howardwalz@gmail.com>
2024-09-05 23:04:13 -0700

    Added documentation to all the publicly availible methods of all files in a good state.

--- Config/CompositeMotorConfig.java
+++ Config/CompositeMotorConfig.java
@@ -1,13 +1,22 @@
 package SOTAlib.Config;
 
+/**
+ * Wrapper class for uniting an EncoderConfig and MotorConfig. Used to create a CompositeMotor.
+ */
 public class CompositeMotorConfig {
     private MotorControllerConfig motorConfig;
     private EncoderConfig encoderConfig;
 
+    /**
+     * @return the MotorControllerConfig object to pass to the factory.
+     */
     public MotorControllerConfig getMotorConfig() {
         return motorConfig;
     }
 
+    /**
+     * @return the EncoderConfig object to pass to the factory.
+     */
     public EncoderConfig getEncoderConfig() {
         return encoderConfig;
     }
